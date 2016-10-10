afdf76a63fc9091074f3cf84f5b498bcd41bf357
Sefton419 <austin.sefton@gmail.com>
2016-10-10 16:37:29 -0700

    [add] creates components to use with main app component

---
+++ src/components/navbar.js
@@ -0,0 +1,17 @@
+const styles = {
+	container: {
+		position: 'static',
+		textAlign: 'center',
+		backgroundColor: 'beige'
+	},
+	title: {
+		display: 'inlineBlock'
+	},
+	question: {
+		position: 'static',
+		width: '50%',
+		margin: 'auto',
+		backgroundColor: 'lightgrey',
+		display: 'inlineBlock'
+	}
+}
